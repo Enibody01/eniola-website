44020eea1e8544e5a5ab4aa064f96478fda803d1
Enibody01 <kayodeeniola7@gmail.com>
2025-06-19 13:06:14 +0100

    Add files via upload

---
+++ login.py
@@ -0,0 +1,9 @@
+username = input("username")
+password = input("password")
+if username == "PETER.A"and password == "peterson":
+    print("Welcome ")
+elif username != "PETER.A" and password == "peterson":
+    print("wrong username")
+elif username == "PETER.A" and password != "peterson":
+    print("wrong password")
+    
